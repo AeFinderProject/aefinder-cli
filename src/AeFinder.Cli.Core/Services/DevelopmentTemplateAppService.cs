265249ee500a0f176550569a01fc69e6f54fabf2
sinclair-aefinder <156668112+sinclair-aefinder@users.noreply.github.com>
2024-08-02 17:10:49 +0800

    Feature: AeFinder Cli tool (#2)
    * feat: add cli
    
    * feat: add ci
    
    * feat: check project name
    
    * feat: remove unused file
    
    * fix: naming
    
    * fix: change key and .sln file name in  sonarqube.yaml
    
    * feat: add GitHub Action for NuGet publish
    
    * feat: improve code
    
    * feat: use ImmutableDictionary
    
    ---------
    
    Co-authored-by: jacob-finder <jacob@aefinder.io>

---
+++ src/AeFinder.Cli.Core/Services/DevelopmentTemplateAppService.cs
@@ -0,0 +1,90 @@
+using System.Text;
+using System.Text.RegularExpressions;
+using AeFinder.Cli.Auth;
+using AeFinder.Cli.Http;
+using AeFinder.Cli.Options;
+using AeFinder.Cli.Utils;
+using Microsoft.Extensions.Logging;
+using Volo.Abp;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Http;
+using Volo.Abp.Json;
+using Volo.Abp.Threading;
+
+namespace AeFinder.Cli.Services;
+
+public class DevelopmentTemplateAppService : IDevelopmentTemplateAppService, ITransientDependency
+{
+    private readonly IAuthService _authService;
+    private readonly IJsonSerializer _jsonSerializer;
+    private readonly CliHttpClientFactory _cliHttpClientFactory;
+    private readonly ICancellationTokenProvider _cancellationTokenProvider;
+    private readonly IRemoteServiceExceptionHandler _remoteServiceExceptionHandler;
+    private readonly ILogger<DevelopmentTemplateAppService> _logger;
+    
+    public DevelopmentTemplateAppService(IAuthService authService, IJsonSerializer jsonSerializer,
+        CliHttpClientFactory cliHttpClientFactory, ICancellationTokenProvider cancellationTokenProvider,
+        ILogger<DevelopmentTemplateAppService> logger, IRemoteServiceExceptionHandler remoteServiceExceptionHandler)
+    {
+        _authService = authService;
+        _jsonSerializer = jsonSerializer;
+        _cliHttpClientFactory = cliHttpClientFactory;
+        _cancellationTokenProvider = cancellationTokenProvider;
+        _logger = logger;
+        _remoteServiceExceptionHandler = remoteServiceExceptionHandler;
+    }
+
+    public async Task CreateProjectAsync(InitAppOptions options)
+    {
+        _logger.LogInformation("Initialize app...");
+        
+        CheckOptions(options);
+        
+        var token = await _authService.GetAccessTokenAsync(options.Network, options.AppId, options.Key);
+        
+        var postData = _jsonSerializer.Serialize(new
+        {
+            Name = options.Name
+        });
+        var url = $"{CliConsts.AeFinderEndpoints[options.Network].ApiEndpoint}api/dev-template";
+
+        var client = _cliHttpClientFactory.CreateClient(token);
+
+        using var response = await client.PostAsync(
+            url,
+            new StringContent(postData, Encoding.UTF8, MimeTypes.Application.Json),
+            _cancellationTokenProvider.Token
+        );
+        
+        await _remoteServiceExceptionHandler.EnsureSuccessfulHttpResponseAsync(response);
+
+        if (options.Directory.IsNullOrWhiteSpace())
+        {
+            options.Directory = Directory.GetCurrentDirectory();
+        }
+            
+        var result = await response.Content.ReadAsStreamAsync();
+        ZipHelper.UnZip(result, options.Directory);
+            
+        _logger.LogInformation("The AeFinder App: {App} is initialized successfully. Directory: {Directory}", options.Name, options.Directory);
+    }
+
+    private static void CheckOptions(InitAppOptions options)
+    {
+        if (options.Name.Length is < 2 or > 20)
+        {
+            throw new UserFriendlyException("The name should be between 2 and 20 in length.");
+        }
+
+        if (!ProjectNameRegex.IsValid(options.Name))
+        {
+            throw new UserFriendlyException("The Name must begin with a letter and can only contain letters('A'-'Z', 'a'-'z'), numbers(0-9), and dots('.').");
+        }
+    }
+}
+
+public static partial class ProjectNameRegex {
+    [GeneratedRegex("^[A-Za-z][A-Za-z0-9.]+")]
+    public static partial Regex Regex();
+    public static bool IsValid(string name) => Regex().IsMatch(name);
+}
